36a7af4ecf982f9d9e2314ae451e04cf4d989ecd
tamastotok <tamastotok.tk@gmail.com>
2021-08-21 18:26:07 +0200

    create server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+const sendMail = require("./mail.js");
+const PORT = process.env.PORT || 8000;
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//  Check server status
+app.get("/status", (req, res) => {
+  res.sendStatus(200);
+});
+
+//  Get data from client submit form
+app.post("/submit", (req, res) => {
+  sendMail(req.body);
+  res.json("You message has been sent successfully!");
+});
+
+//  Init server
+app.listen(PORT, () => {
+  console.log("Server is running on port: ", PORT);
+});
